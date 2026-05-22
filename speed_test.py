33991fdb6bd3a4fdc518f2a2fbbe766ce6068685
Dhanuskiruthick <dhanuskiruthick@gmail.com>
2026-05-22 19:57:58 +0530

    Update speed_test.py

--- speed_test.py
+++ speed_test.py
@@ -1,32 +1,20 @@
+import time
 import hashlib
 import bcrypt
-import time
 
-#===========================
-# SHA256 TEST
-#===========================
+def run_speed_test():
+    print("\nSHA256 Test Running...")
+    start = time.time()
 
-start = time.time()
+    for i in range(100000):
+        hashlib.sha256(f"test{i}".encode()).hexdigest()
 
-for i in range(100000):
-    hashlib.sha256(f"password{i}".encode()).hexdigest()
+    print("SHA256 Time:", time.time() - start)
 
-end = time.time()
+    print("\nBCRYPT Test Running...")
+    start = time.time()
 
-print("\nSHA256 Time: ")
-print(end - start)
+    for i in range(10):
+        bcrypt.hashpw(f"test{i}".encode(), bcrypt.gensalt())
 
-
-#===========================
-# BCRYPT TEST   
-#===========================
-
-start = time.time()
-
-for i in range(10):  # bcrypt is much slower than sha256, so we will do only 10 iterations to keep the test reasonable
-    bcrypt.hashpw(f"password{i}".encode(), bcrypt.gensalt())
-
-end = time.time()
-
-print("\nBCRYPT Time: ")
-print(end - start)
+    print("BCRYPT Time:", time.time() - start)
